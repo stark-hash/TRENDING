5458198724cc5f5f6275eedb269f369217d51578
stark-hash <91049862+stark-hash@users.noreply.github.com>
2024-06-22 08:35:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -39,7 +39,7 @@ class Config(object):
     START_PIC   = os.environ.get("START_PIC", "")
     ADMIN       = os.environ.get("API_ID", "")
     FORCE_SUB   = os.environ.get("FORCE_SUB", "") 
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002211649043"))
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", None))
 
     # wes response configuration     
     WEBHOOK = bool(os.environ.get("WEBHOOK", "True"))
